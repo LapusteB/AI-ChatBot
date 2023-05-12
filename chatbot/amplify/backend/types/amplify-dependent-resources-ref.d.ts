1089c469ddb65226b2b072793bd6a80acd3f1845
Brian Lapuste <67889855+LapusteB@users.noreply.github.com>
2023-05-12 13:03:36 -0600

    Add RESTapi, Lambda Function, Backend Dependencies: AWS UI Tools

--- chatbot/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ chatbot/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api6b706c74": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "chatbot86173404": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import logo2 from './OA.png'
 import React, { useState } from 'react';
-
+import { Amplify } from 'aws-amplify';
+import { API } from 'aws-amplify';
 import {TextField, Button} from '@mui/material'
+
+const myAPI = "api6b706c74"
+const path = '/prompt'
+
+
 
 function App() {
   const [prompt, setPrompt] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log('Handling form submission');
     // Perform submit logic here
     console.log('Form submitted:', prompt);
     // Reset the input field
